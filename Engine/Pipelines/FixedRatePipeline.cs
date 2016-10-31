0a5bdcf5d87dd90998280db2942748944aa9dbea
Silent Penguin <silentpneguin@local>
2016-10-31 00:18:42 +0000

    Made a Sequential pipeline to act as the base game control.

--- Engine/Pipelines/FixedRatePipeline.cs
+++ Engine/Pipelines/FixedRatePipeline.cs
@@ -9,13 +9,14 @@ namespace Engine.Pipelines
             frameDuration = TimeSpan.FromTicks(1 / framerate * TimeSpan.TicksPerMillisecond);
         }
 
+        public Func<FrameInfo, bool> action;
+        private FrameInfo nextFixedFrame;
         private TimeSpan frameDuration;
-        public FrameInfo nextFixedFrame { get; private set; }
 
         public override bool ProcessFrame(FrameInfo frame)
         {
             if (nextFixedFrame.Time > frame.Time) return false;
-            action();
+            action(nextFixedFrame);
             LastProcessedFrame = nextFixedFrame;
             nextFixedFrame = new FrameInfo { Time = nextFixedFrame.Time + frameDuration, TimeSinceLastFrame = frameDuration };
             return true;
