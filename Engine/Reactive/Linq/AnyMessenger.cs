f7ace2e9073fe8d3bcefed210598729a5e4b459e
Silent Penguin <silentpneguin@local>
2016-11-04 14:19:18 +0000

    Less invasive behaviour for Messenger.

--- Engine/Reactive/Linq/AnyMessenger.cs
+++ Engine/Reactive/Linq/AnyMessenger.cs
@@ -4,12 +4,15 @@ namespace Sparrow.Reactive.Linq
 {
     public class AnyMessenger<T> : Messenger<T, bool>
     {
-        public AnyMessenger(ISender<T> sender, Func<T, bool> predictate) : base(predictate)
+        public AnyMessenger(ISender<T> sender, Func<T, bool> predictate) : base(null)
         {
+            Predicate = predictate;
             Value = true;
             sender.Attach(this);
         }
+
+        private Func<T, bool> Predicate;
         
-        public override void Receive(T value) { Send(Value || Action(value)); }
+        public override void Receive(T value) { Send(Value || Predicate(value)); }
     }
 }
