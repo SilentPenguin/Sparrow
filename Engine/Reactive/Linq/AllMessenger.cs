dd424c6db028884399a2c25397a6757cd4ab527b
SilentPenguin <silentpenguin@hotmail.co.uk>
2016-11-04 14:19:18 +0000

    Less invasive behaviour for Messenger.

--- Engine/Reactive/Linq/AllMessenger.cs
+++ Engine/Reactive/Linq/AllMessenger.cs
@@ -4,12 +4,15 @@ namespace Sparrow.Reactive.Linq
 {
     public class AllMessenger<T> : Messenger<T, bool>
     {
-        public AllMessenger(ISender<T> sender, Func<T, bool> predictate) : base(predictate)
+        public AllMessenger(ISender<T> sender, Func<T, bool> predictate) : base(null)
         {
+            Predicate = predictate;
             Value = true;
             sender.Attach(this);
         }
+
+        private Func<T, bool> Predicate;
         
-        public override void Receive(T value) { Send(Value && Action(value)); }
+        public override void Receive(T value) { Send(Value && Predicate(value)); }
     }
 }
--- Engine/Reactive/Linq/SelectMessenger.cs
+++ Engine/Reactive/Linq/SelectMessenger.cs
@@ -4,9 +4,14 @@ namespace Sparrow.Reactive.Linq
 {
     public class SelectMessenger<TSource, TResult> : Messenger<TSource, TResult>
     {
-        public SelectMessenger(ISender<TSource> sender, Func<TSource, TResult> predictate) : base(predictate)
+        public SelectMessenger(ISender<TSource> sender, Func<TSource, TResult> selector) : base(null)
         {
+            Selector = selector;
             sender.Attach(this);
         }
+
+        private Func<TSource, TResult> Selector;
+        
+        public override void Receive(TSource value) { Send(Selector(value)); }
     }
 }
