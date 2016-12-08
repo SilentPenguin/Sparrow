103f7a51d9e04aec4a891cb44f4334359049982f
SilentPenguin <silentpenguin@hotmail.co.uk>
2016-12-08 16:17:00 +0000

    Added Mesh structure

---
+++ Engine/Graphics/Mesh.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Sparrow.Graphics
+{
+    public class Mesh
+    {
+        public IList<Triangle> Triangles { get; private set; } = new List<Triangle>();
+        
+        internal float[] VertexArray
+        {
+            get
+            {
+                var i = 0;
+                return Triangles
+                    .SelectMany(t => t.Vertices)
+                    .Distinct()
+                    .SelectMany(v => { v.Index = i++; return v.Position.items; })
+                    .ToArray();
+            }
+        }
+
+        internal int[] IndexArray
+        {
+            get
+            {
+                return Triangles
+                    .SelectMany(t => t.Vertices)
+                    .Select(v => v.Index)
+                    .ToArray();
+            }
+        }
+    }
+}
