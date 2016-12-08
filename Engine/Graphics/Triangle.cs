24cb4d9036fad6c17f691c8ef7e92729299f040d
Silent Penguin <silentpneguin@local>
2016-12-08 16:17:00 +0000

    Added Mesh structure

---
+++ Engine/Graphics/Triangle.cs
@@ -0,0 +1,7 @@
+namespace Sparrow.Graphics
+{
+    public class Triangle
+    {
+        public Vertex[] Vertices { get; private set; } = new Vertex[3];
+    }
+}
---
+++ Engine/Graphics/Vertex.cs
@@ -0,0 +1,10 @@
+using Sparrow.Numerics;
+
+namespace Sparrow.Graphics
+{
+    public class Vertex
+    {
+        internal int Index{ get; set; }
+        public Vector<float> Position { get; set; }
+    }
+}
