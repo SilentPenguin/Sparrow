2f5cc6632ea7701ec5a8b0cbb330a1f88260c254
Silent Penguin <silentpneguin@local>
2016-12-09 16:01:38 +0000

    Reorganised each subcomponent into seperate projects.

--- Engine/Pipelines/PipelineBase.cs
+++
@@ -1,8 +0,0 @@
-namespace Sparrow.Pipelines
-{
-    public abstract class PipelineBase
-    {
-        public FrameInfo LastProcessedFrame { get; protected set; }
-        public abstract void ProcessFrame(FrameState frame);
-    }
-}
