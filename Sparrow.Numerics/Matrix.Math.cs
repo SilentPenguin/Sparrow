01fdc9cd5c99549e58b270494930c61d4aa5dade
SilentPenguin <silentpenguin@hotmail.co.uk>
2016-12-20 21:26:36 +0000

    Numerics classes return zero for out of range.

--- Sparrow.Numerics/Matrix.Math.cs
+++ Sparrow.Numerics/Matrix.Math.cs
@@ -6,14 +6,20 @@ namespace Sparrow.Numerics
     {
         abstract class Math<S> where S : struct
         {
+            public abstract S Get(Matrix<S> a, int x, int y);
             public abstract Matrix<S> Mul(Matrix<S> a, Matrix<S> b);
             public abstract Vector<S> Mul(Matrix<S> a, Vector<S> b);
-            public abstract Matrix<S> Identity(int size);
             public abstract Matrix<S> Empty(int width, int height);
         }
         
         class MathFloat : Math<float>
         {
+            public override float Get(Matrix<float> a, int x, int y)
+            {
+                return x < a.Width && y < a.Height
+                    ? a[x, y] : (x == y ? 1 : 0);
+            }
+
             public override Matrix<float> Mul(Matrix<float> a, Matrix<float> b)
             {
                 if (a.Height != b.Width) throw new ArgumentException();
@@ -33,14 +39,6 @@ namespace Sparrow.Numerics
                 return new Vector<float>(result);
             }
 
-            public override Matrix<float> Identity(int size)
-            {
-                var result = new float[size, size];
-                for (int i = size; i-- != 0;)
-                    result[i, i] = 1;
-                return new Matrix<float>(result);
-            }
-
             public override Matrix<float> Empty(int width, int height)
             {
                 var result = new float[width, height];
@@ -50,6 +48,12 @@ namespace Sparrow.Numerics
         
         class MathDouble : Math<double>
         {
+            public override double Get(Matrix<double> a, int x, int y)
+            {
+                return x < a.Width && y < a.Height
+                    ? a[x, y] : (x == y ? 1 : 0);
+            }
+
             public override Matrix<double> Mul(Matrix<double> a, Matrix<double> b)
             {
                 if (a.Height != b.Width) throw new ArgumentException();
@@ -67,14 +71,6 @@ namespace Sparrow.Numerics
                 for (int i = result.Length; i-- != 0;)
                     result[i] = Vector<double>.Dot(a.Row(i), b);
                 return new Vector<double>(result);
-            }
-
-            public override Matrix<double> Identity(int size)
-            {
-                var result = new double[size, size];
-                for (int i = size; i-- != 0;)
-                    result[i, i] = 1;
-                return new Matrix<double>(result);
             }
 
             public override Matrix<double> Empty(int width, int height)
