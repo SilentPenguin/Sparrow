1ac16e46c39fcb51c0a2a0e9ce3a43a83d682879
Silent Penguin <silentpneguin@local>
2017-01-20 15:11:52 +0000

    mixed tabs and spaces

--- Sparrow.SDL2/Sdl.Events.cs
+++ Sparrow.SDL2/Sdl.Events.cs
@@ -7,38 +7,38 @@ namespace Sparrow.Sdl
     {
 
         public enum EventType : uint
-		{
-			FirstEvent = 0,
+        {
+            FirstEvent = 0,
 
-			/* Application events */
-			Quit = 0x100,
+            /* Application events */
+            Quit = 0x100,
 
-			/* Window events */
-			WindowEvent = 0x200,
+            /* Window events */
+            WindowEvent = 0x200,
             SysWmEvent,
 
-			/* Keyboard events */
-			KeyDown = 0x300,
-			KeyUp,
-			TextEditing,
-			TextInput,
+            /* Keyboard events */
+            KeyDown = 0x300,
+            KeyUp,
+            TextEditing,
+            TextInput,
 
-			/* Mouse events */
-			MouseMotion = 0x400,
-			MouseButtonDown,
-			MouseButtonUp,
-			MouseWheel,
+            /* Mouse events */
+            MouseMotion = 0x400,
+            MouseButtonDown,
+            MouseButtonUp,
+            MouseWheel,
 
-			/* Joystick events */
-			JoyAxisMotion = 0x600,
-			JoyBallMotion,
-			JoyHatMotion,
-			JoyButtonDown,
-			JoyButtonUp,
-			JoyDeviceAdded,
-			JoyDeviceRemoved,
+            /* Joystick events */
+            JoyAxisMotion = 0x600,
+            JoyBallMotion,
+            JoyHatMotion,
+            JoyButtonDown,
+            JoyButtonUp,
+            JoyDeviceAdded,
+            JoyDeviceRemoved,
 
-			/* Game controller events */
+            /* Game controller events */
             ControllerAxisMotion = 0x650,
             ControllerButtonDown,
             ControllerButtonUp,
@@ -46,108 +46,108 @@ namespace Sparrow.Sdl
             ControllerDeviceRemoved,
             ControllerDeviceRemapped,
 
-			/* Touch events */
+            /* Touch events */
             FingerDown = 0x700,
             FingerUp,
             FingerMotion,
 
-			/* Gesture events */
+            /* Gesture events */
             DollarGesture = 0x800,
             DollarRecord,
             MultiGesture,
 
-			/* Clipboard events */
+            /* Clipboard events */
             ClipboardUpdate = 0x900,
 
-			/* Drag and drop events */
+            /* Drag and drop events */
             DropFile = 0x1000,
-			/* Only available in 2.0.4 or higher */
+            /* Only available in 2.0.4 or higher */
             DropText,
             DropBegin,
             DropComplete,
 
-			/* Audio hotplug events */
-			/* Only available in SDL 2.0.4 or higher */
-			AudioDeviceAdded = 0x1100,
-			AudioDeviceRemoved,
+            /* Audio hotplug events */
+            /* Only available in SDL 2.0.4 or higher */
+            AudioDeviceAdded = 0x1100,
+            AudioDeviceRemoved,
 
-			/* Render events */
-			/* Only available in SDL 2.0.2 or higher */
+            /* Render events */
+            /* Only available in SDL 2.0.2 or higher */
             RenderTargetsReset = 0x2000,
-			/* Only available in SDL 2.0.4 or higher */
+            /* Only available in SDL 2.0.4 or higher */
             RenderDeviceReset,
 
-			/* Events SDL_USEREVENT through SDL_LASTEVENT are for
-			 * your use, and should be allocated with
-			 * SDL_RegisterEvents()
-			 */
-			UserEvent = 0x8000,
+            /* Events SDL_USEREVENT through SDL_LASTEVENT are for
+            * your use, and should be allocated with
+            * SDL_RegisterEvents()
+            */
+            UserEvent = 0x8000,
 
-			/* The last event, used for bouding arrays. */
-			LastEvent = 0xFFFF
-		}
+            /* The last event, used for bouding arrays. */
+            LastEvent = 0xFFFF
+        }
 
         [StructLayout(LayoutKind.Sequential)]
-		public struct QuitEvent
-		{
-			public EventType type;
-			public UInt32 timestamp;
-		}
+        public struct QuitEvent
+        {
+            public EventType type;
+            public UInt32 timestamp;
+        }
 
-		[StructLayout(LayoutKind.Explicit)]
-		public struct Event
-		{
-			[FieldOffset(0)]
-			public EventType type;
-			/*
+        [StructLayout(LayoutKind.Explicit)]
+        public struct Event
+        {
             [FieldOffset(0)]
-			public WindowEvent window;
-			[FieldOffset(0)]
-			public KeyboardEvent key;
-			[FieldOffset(0)]
-			public TextEditingEvent edit;
-			[FieldOffset(0)]
-			public TextInputEvent text;
-			[FieldOffset(0)]
-			public MouseMotionEvent motion;
-			[FieldOffset(0)]
-			public MouseButtonEvent button;
-			[FieldOffset(0)]
-			public MouseWheelEvent wheel;
-			[FieldOffset(0)]
-			public JoyAxisEvent jaxis;
-			[FieldOffset(0)]
-			public JoyBallEvent jball;
-			[FieldOffset(0)]
-			public JoyHatEvent jhat;
-			[FieldOffset(0)]
-			public JoyButtonEvent jbutton;
-			[FieldOffset(0)]
-			public JoyDeviceEvent jdevice;
-			[FieldOffset(0)]
-			public ControllerAxisEvent caxis;
-			[FieldOffset(0)]
-			public ControllerButtonEvent cbutton;
-			[FieldOffset(0)]
-			public ControllerDeviceEvent cdevice;
-            */
-			[FieldOffset(0)]
-			public QuitEvent quit;
+            public EventType type;
             /*
-			[FieldOffset(0)]
-			public UserEvent user;
-			[FieldOffset(0)]
-			public SysWMEvent syswm;
-			[FieldOffset(0)]
-			public TouchFingerEvent tfinger;
-			[FieldOffset(0)]
-			public MultiGestureEvent mgesture;
-			[FieldOffset(0)]
-			public DollarGestureEvent dgesture;
-			[FieldOffset(0)]
-			public DropEvent drop;
+            [FieldOffset(0)]
+            public WindowEvent window;
+            [FieldOffset(0)]
+            public KeyboardEvent key;
+            [FieldOffset(0)]
+            public TextEditingEvent edit;
+            [FieldOffset(0)]
+            public TextInputEvent text;
+            [FieldOffset(0)]
+            public MouseMotionEvent motion;
+            [FieldOffset(0)]
+            public MouseButtonEvent button;
+            [FieldOffset(0)]
+            public MouseWheelEvent wheel;
+            [FieldOffset(0)]
+            public JoyAxisEvent jaxis;
+            [FieldOffset(0)]
+            public JoyBallEvent jball;
+            [FieldOffset(0)]
+            public JoyHatEvent jhat;
+            [FieldOffset(0)]
+            public JoyButtonEvent jbutton;
+            [FieldOffset(0)]
+            public JoyDeviceEvent jdevice;
+            [FieldOffset(0)]
+            public ControllerAxisEvent caxis;
+            [FieldOffset(0)]
+            public ControllerButtonEvent cbutton;
+            [FieldOffset(0)]
+            public ControllerDeviceEvent cdevice;
             */
-		}
+            [FieldOffset(0)]
+            public QuitEvent quit;
+            /*
+            [FieldOffset(0)]
+            public UserEvent user;
+            [FieldOffset(0)]
+            public SysWMEvent syswm;
+            [FieldOffset(0)]
+            public TouchFingerEvent tfinger;
+            [FieldOffset(0)]
+            public MultiGestureEvent mgesture;
+            [FieldOffset(0)]
+            public DollarGestureEvent dgesture;
+            [FieldOffset(0)]
+            public DropEvent drop;
+*/
+        }
 
         [DllImport(DllName, EntryPoint = "SDL_PollEvent")]
         public static extern uint PollEvent(out Event _event);
