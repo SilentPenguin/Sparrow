dc32846325564bd8856af35510af1c3571c0c6df
Silent Penguin <silentpneguin@local>
2017-01-21 15:50:19 +0000

    Added documentation for sdl2

--- Sparrow.SDL2/Sdl.Render.cs
+++ Sparrow.SDL2/Sdl.Render.cs
@@ -5,6 +5,10 @@ namespace Sparrow.Sdl2
 {
     public static partial class Sdl
     {
+        /// <summary>
+		/// The types of renderer that can be created with <see cref='CreateRenderer()'/>
+		/// </summary>
+        /// <seealso cref="CreateRenderer()"/>
         [Flags]
         public enum RendererFlags : uint
         {
@@ -14,9 +18,29 @@ namespace Sparrow.Sdl2
             TargetTexture = 0x00000008
         }
 
+        /// <summary>
+        /// Create a 2D rendering context for a window.
+        /// </summary>
+        /// <param name="window">The window where rendering is displayed</param>
+        /// <param name="index">The index of the rendering driver to initialize, omit to initialize the first one supporting the requested flags</param>
+        /// <param name="flags">Zero or more <see cref'RendererFlags'/></param>
+        /// <returns>Returns a valid rendering context or null if there was an error.</returns>
+        /// <remarks>
+        /// Flags may be any combination of <see cref='RendererFlags'/>.
+        /// Providing no flags gives priority to available SDL_RENDERER_ACCELERATED renderers.
+        /// </remarks>
+        /// <seealso cref="CreateSoftwareRenderer()"/>
+        /// <seealso cref="DestroyRenderer()"/>
+        /// <seealso cref="GetNumRenderDrivers()"/>
+        /// <seealso cref="GetRendererInfo()"/>
         [DllImport(DllName, EntryPoint = "SDL_CreateRenderer")]
         public static extern IntPtr CreateRenderer(IntPtr window, int index = -1, RendererFlags flags = 0);
 
+        /// <summary>
+        /// Destroy the rendering context for a window and free associated textures.
+        /// </summary>
+        /// <param name="renderer">A previously created rendering context.</param>
+        /// <seealso cref="CreateRenderer()"/>
         [DllImport(DllName, EntryPoint = "SDL_DestroyRenderer")]
         public static extern void DestroyRenderer(IntPtr renderer);
     }
--- Sparrow.SDL2/Sdl.Timer.cs
+++ Sparrow.SDL2/Sdl.Timer.cs
@@ -4,6 +4,11 @@ namespace Sparrow.Sdl2
 {
     public static partial class Sdl
     {
+        /// <summary>
+        /// Wait a specified number of milliseconds before returning.
+        /// </summary>
+        /// <param name="ms">the number of milliseconds to delay</param>
+        /// <remarks>This function waits at least the specified time, but possibly longer due to OS scheduling.</remarks>
         [DllImport(DllName, EntryPoint = "SDL_Delay")]
         public static extern void Delay(uint ms);
     }
