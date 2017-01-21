4b74aaf7fab4fd8cc10030f0afa5dacd24c8e0a3
SilentPenguin <silentpenguin@hotmail.co.uk>
2017-01-21 16:13:06 +0000

    Documented the Engine class.

--- Sparrow/Engine.cs
+++ Sparrow/Engine.cs
@@ -4,11 +4,29 @@ using System.Collections.Generic;
 
 namespace Sparrow
 {
+    /// <summary>
+    /// Engine contains the core of sparrow.
+    /// </summary>
+    /// <remarks>
+    /// It is responsible for continuously generating FrameState objects.
+    /// Frame states are then passed to the action delegate which is responsible
+    /// for marshalling the subsystems within the engine.
+    /// </remarks>
     public class Engine
     {
+        /// <summary>
+        /// action is the main function to be called by the gameloop.
+        /// </summary>
         public Action<FrameState> action;
+
+        /// <summary>
+        /// frameGenerator returns a new frame state object on each loop.
+        /// </summary>
         private IEnumerable<FrameState> frameGenerator = new FrameGenerator();
 
+        /// <summary>
+        /// Loop is the main game loop
+        /// </summary>
         public void Loop()
         {
             foreach (var frame in frameGenerator)
